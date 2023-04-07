447bd48fe0bb9d4f9c03e89a663720e7e8f9289c
leonardosakay <bartlett.leonardo@gmail.com>
2023-04-07 01:38:57 -0400

    Version1

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+"""
+Title: COVID-19 Data Retrieval and Database Loading Script
+Author: Leo Bartlett
+Date: April 7, 2023
+Version: 1.0
+
+This Python script retrieves COVID-19 data from the CDC API and loads it into a SQL Server database.
+It utilizes the 'requests', 'json', 'pandas', 'datetime', and 'pyodbc' libraries to make API requests,
+parse response data, transform the data into a Pandas dataframe, and load it into a SQL Server database.
+The script is specifically designed to run on the LeoBartlett server, which is a localhost server.
+If you wish to run this script on a different server, you may need to modify the 'SERVER' and 'DATABASE' parameters in the 'pyodbc.connect'
+function accordingly.
+To update the data on the 'CovidData' table if new data is added or data is removed, the script truncates the table before inserting new data.
+
+"""
+
 import requests
 import json
 import pandas as pd
@@ -7,7 +23,7 @@ import pyodbc
 # Define API endpoint and parameters
 endpoint = "https://data.cdc.gov/resource/9mfq-cb36.json"
 params = {'$select': 'submission_date,state,tot_cases,tot_death',
-          '$where': 'submission_date >= "2022-01-01T00:00:00.000"', '$limit': 100000}
+          '$where': 'submission_date >= "2020-01-01T00:00:00.000"'}
 
 # Make API request and parse response data
 response = requests.get(endpoint, params=params)
@@ -22,17 +38,21 @@ conn = pyodbc.connect('Trusted_Connection=yes', DRIVER='{SQL Server}', SERVER='L
 cursor = conn.cursor()
 
 # Check if table exists
-table_exists = cursor.execute("SELECT COUNT(*) FROM sys.tables WHERE name='covid_data'").fetchone()[0]
+table_exists = cursor.execute("SELECT COUNT(*) FROM sys.tables WHERE name='CovidData'").fetchone()[0]
 
-# Create SQL Server table (if it doesn't exist)
-if not table_exists:
+# If table exists, truncate it to remove existing data
+if table_exists:
+    cursor.execute("TRUNCATE TABLE CovidData")
+    print("Updating table!")
+# If table does not exist, create it
+else:
     cursor.execute(
-        "CREATE TABLE covid_data (submission_date datetime, state varchar(255), tot_cases int, tot_death int, timestamp datetime)")
-
+        "CREATE TABLE CovidData (SubmissionDate datetime, state varchar(255), TotalCases int, TotalDeaths int, timestamp datetime)")
+    print("Table Created Successfully!")
 # Insert data into SQL Server table
 for index, row in df.iterrows():
     cursor.execute(
-        "INSERT INTO covid_data (submission_date, state, tot_cases, tot_death, timestamp) VALUES (?, ?, ?, ?, ?)",
+        "INSERT INTO CovidData (SubmissionDate, state, TotalCases , TotalDeaths , timestamp) VALUES (?, ?, ?, ?, ?)",
         row['submission_date'], row['state'], row['tot_cases'], row['tot_death'], row['timestamp'])
 conn.commit()
 
